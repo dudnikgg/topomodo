34850f0c2381a359aea108029e205138d0f89dfb
Denis Dudnik <denis.dudnik@resultify.se>
2022-10-17 15:13:46 +0300

    add todoItem with methods

--- src/components/Todo/TodoItem.vue
+++ src/components/Todo/TodoItem.vue
@@ -1,9 +1,15 @@
 <script lang="ts" setup>
+import type { ITodoItem } from '@/types/TodoItem'
+const { todo } = defineProps<{
+    todo: ITodoItem
+}>()
 
 </script>
 <template>
-    <div class="todo__item">
-        TodoItem
+    <div class="c-todo__item">
+        <button class="c-todo__button" @click="$emit('toggleTodo', todo.id, !todo.done)" data-test="toggleTodo">{{todo.done ? '!': '✓' }}</button>
+        <p class="c-todo__text">{{todo.text}}</p>
+        <button class="c-todo__button" @click="$emit('removeTodo', todo.id)" data-test="removeTodo">x</button>
     </div>
 </template>
 <style scoped>
