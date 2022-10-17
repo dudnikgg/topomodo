e3b54903f1801d7f5b6fdc79b6f75e16ea9b7cdc
Denis Dudnik <denis.dudnik@resultify.se>
2022-10-17 15:13:53 +0300

    add todoItem tests

--- src/components/Todo/TodoItem.test.ts
+++ src/components/Todo/TodoItem.test.ts
@@ -4,8 +4,65 @@ import { expect, it, describe } from "vitest"
 import TodoItem from "./TodoItem.vue"
 
 describe("TodoItem.vue test", () => {
-  it("Show correct text 'TodoItem'", () =>{
-    const wrapper = shallowMount(TodoItem)
-    expect(wrapper.text()).toMatch("TodoItem")
+  it("Toggle todo done/not done", async () => {
+    const wrapper = Wrapper({
+      todo: {
+        id: 1,
+        text: "todo1",
+        done: false,
+      },
+    })
+    wrapper.find("button[data-test='toggleTodo']").trigger("click")
+
+    expect(wrapper.emitted("toggleTodo")).toHaveLength(1)
+    expect(wrapper.emitted("toggleTodo")[0]).toEqual([1, true])
+
+    const wrapper2 = Wrapper({
+      todo: {
+        id: 1,
+        text: "todo1",
+        done: true,
+      },
+    })
+    wrapper2.find("button[data-test='toggleTodo']").trigger("click")
+
+    expect(wrapper2.emitted("toggleTodo")).toHaveLength(1)
+    expect(wrapper2.emitted("toggleTodo")[0]).toEqual([1, false])
+  })
+
+  it("Emits a remove event with todo id", async () => {
+    const wrapper = Wrapper({
+      todo: {
+        id: 1,
+        text: "todo1",
+        done: false,
+      },
+    })
+    wrapper.find("button[data-test='removeTodo']").trigger("click")
+
+    expect(wrapper.emitted("removeTodo")).toHaveLength(1)
+  })
+
+  it("Emits a pomodoro event (pomodoros estimate) with todo id", async () => {
+    const wrapper = Wrapper({
+      todo: {
+        id: 1,
+        text: "todo1",
+        done: false,
+        pomodoros: 3
+      }
+    })
+
+    wrapper.find("button[data-test='pomodorosTodo']").trigger("click")
+
+    expect(wrapper.emitted("pomodorosTodo")).toHaveLength(1)
   })
 })
+
+function Wrapper(props: object) {
+  return shallowMount(TodoItem, {
+    props: {
+      ...props,
+    },
+  })
+}
