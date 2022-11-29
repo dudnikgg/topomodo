ee06bc40f48713c20f79764bfd1648b02f08da2a
Denys Dudnik <dudnik.gg@gmail.com>
2022-11-29 13:25:57 +0200

    add radial-progress vue component - merge scripts for dev and test - add pinia store

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#l-app')
+createApp(App).use(createPinia()).mount('#l-app')
